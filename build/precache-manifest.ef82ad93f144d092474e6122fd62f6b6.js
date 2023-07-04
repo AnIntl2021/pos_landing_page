ae4e52a9d381215518895cd6d12be138f3ab38a7
Joginder Singh <anintl.ind@gmail.com>
2023-07-04 16:59:34 +0530

    landing page build

---
+++ build/precache-manifest.ef82ad93f144d092474e6122fd62f6b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec23a35f2d166d44df6757bd8b49bc83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6846dc2732e2327a8c76",
+    "url": "/static/css/2.777d1846.chunk.css"
+  },
+  {
+    "revision": "bc45068c2675f32dd322",
+    "url": "/static/css/main.ace710f2.chunk.css"
+  },
+  {
+    "revision": "6846dc2732e2327a8c76",
+    "url": "/static/js/2.feab8635.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.feab8635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc45068c2675f32dd322",
+    "url": "/static/js/main.3761c575.chunk.js"
+  },
+  {
+    "revision": "0ece359e4c77dc324806",
+    "url": "/static/js/runtime-main.8587fc2c.js"
+  },
+  {
+    "revision": "cd0a54ff64f80ed569f20404ce5a3209",
+    "url": "/static/media/loginpageimage.cd0a54ff.png"
+  },
+  {
+    "revision": "9bb451511cdebf5545335c4da6996c5a",
+    "url": "/static/media/reactback.9bb45151.png"
+  }
+]);
