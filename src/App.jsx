42e32911ec05a420c4e4d6c22c30d643ca7c0b71
Joginder Singh <anintl.ind@gmail.com>
2023-07-04 15:24:18 +0530

    web landing page done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,71 @@
-import "./styles.css";
-import LoginForm from "./components/login_form";
+import React, { useEffect, useState } from "react"
+import "./styles/main.css";
+// import LoginForm from "./components/login_form";
+import LandingForm from "./components/landing_form";
+import featureImg from './assets/loginpageimage.png';
+import { useWindowDimensions } from './components/utils.js';
 
 function App() {
+
+  // let width = 1000;
+
+  const [width, setWidth] = useState(1000);
+
+
+  useEffect(() => {
+    // width = useWindowDimensions();
+    setWidth(window.innerWidth);
+    console.log('width: ', width);
+    const script = document.createElement("script")
+    script.id = 'wf_anal';
+    script.src =
+      "https://crm.zohopublic.com/crm/WebFormAnalyticsServeServlet?rid=261ecd957ff9ba152b8b186a9bd4728895298b3fb3b654458c494237be5ca692gidfbd9ac9ef083d47d156e2b601ccf851b002f8770fc457789a46bb8163f949f31gid885e3c1045bd9bdcc91bdf30f82b5696gid14f4ec16431e0686150daa43f3210513&tw=61690b96c1d0471b638f31426f38e68aa67fb7ed6da86f32dc10ad817fe55a0a"
+
+    script.async = true
+  }, [width]);
+
+
+
+  const coverDiv = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    height: '100vh',
+    width: '200vh',
+  };
+
+  const imgStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '80vh',
+    width: '100vh',
+  };
+
+  const formStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '50vh',
+    width: '180vh',
+  };
+
   return (
     <>
-      <LoginForm />
+      {width > 700 ?
+        (<div style={coverDiv}>
+          {/* <div><p>More than 600</p></div> */}
+
+
+          <img style={imgStyle} src={featureImg} alt={'Feature'} />
+
+          <div style={formStyle}>
+            <LandingForm />
+          </div>
+
+
+        </div>) : (<div><p>More than 600</p></div>)}
     </>
   );
 }
--- src/components/login_form.js
+++ src/components/login_form.js
@@ -1,149 +1,118 @@
-import { useState } from "react";
-import logo from '../assets/loginpageimage.png';
+// import { useState } from "react";
+// import logo from '../assets/loginpageimage.png';
 import "../styles.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+// import { Form } from "react-bootstrap";
 
 
-function LoginForm() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+const LoginForm = () => {
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
-
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
+  function tooltipShow5813716000000450005(el) {
+    var tooltip = el.nextElementSibling;
+    var tooltipDisplay = tooltip.style.display;
+    if (tooltipDisplay == 'none') {
+      var allTooltip = document.getElementsByClassName('zcwf_tooltip_over');
+      for (let i = 0; i < allTooltip.length; i++) {
+        allTooltip[i].style.display = 'none';
       }
+      tooltip.style.display = 'block';
     } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
+      tooltip.style.display = 'none';
     }
-  };
+  }
 
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
+  // function checkMandatory5813716000000450005() {
+  //   var mndFileds = new Array('Company', 'Last Name');
+  //   var fldLangVal = new Array('Company', 'Last\x20Name');
+  //   for (let i = 0; i < mndFileds.length; i++) {
+  //     var fieldObj = document.forms['WebToLeads5813716000000450005'][mndFileds[i]];
+  //     if (fieldObj) {
+  //       if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length == 0) {
+  //         if (fieldObj.type == 'file') {
+  //           alert('Please select a file to upload.');
+  //           fieldObj.focus();
+  //           return false;
+  //         }
+  //         alert(fldLangVal[i] + ' cannot be empty.');
+  //         fieldObj.focus();
+  //         return false;
+  //       } else if (fieldObj.nodeName == 'SELECT') {
+  //         if (fieldObj.options[fieldObj.selectedIndex].value == '-None-') {
+  //           alert(fldLangVal[i] + ' cannot be none.');
+  //           fieldObj.focus();
+  //           return false;
+  //         }
+  //       } else if (fieldObj.type == 'checkbox') {
+  //         if (fieldObj.checked == false) {
+  //           alert('Please accept  ' + fldLangVal[i]);
+  //           fieldObj.focus();
+  //           return false;
+  //         }
+  //       }
+  //       try {
+  //         if (fieldObj.name == 'Last Name') {
+  //           name = fieldObj.value;
+  //         }
+  //       } catch (e) { }
+  //     }
+  //   }
+  //   if (!validateEmail5813716000000450005()) { return false; }
+  //   document.querySelector('.crmWebToEntityForm .formsubmit').setAttribute('disabled', true);
+  // }
 
-  // JSX code for login form
-  const renderForm = (
-    <div className="row my-5">
-      <div className="col-6 ">
-        <img src={logo} className="img-responsive img-fluid" />
-      </div>
+  function validateEmail5813716000000450005() {
+    // var form = document.forms['WebToLeads5813716000000450005'];
+    // var emailFld = form.querySelectorAll('[ftype=email]');
+    // var i;
+    // for (i = 0; i < emailFld.length; i++) {
+    //   var emailVal = emailFld[i].value;
+    //   if ((emailVal.replace(/^\s+|\s+$/g, '')).length != 0) {
+    //     var atpos = emailVal.indexOf('@');
+    //     var dotpos = emailVal.lastIndexOf('.');
+    //     if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= emailVal.length) {
+    //       alert('Please enter a valid email address. ');
+    //       emailFld[i].focus();
+    //       return false;
+    //     }
+    //   }
+    // }
+    return true;
+  }
 
-      <div className="col-6 card p-5 container">
-        <form onSubmit={handleSubmit}>
-          <div className="fs-1 fw-bold " >{"Get In Touch"}</div>
-          <div className="row justify-content-center align-items-center g-2">
-            <div className="col">
-              <div className="input-container form-label">
-                <label>Username </label>
-                <input type="email" className="form-control" name="uname" required />
-                {renderErrorMessage("uname")}
-              </div>
-            </div>
-            <div className="col">
-              <div className="input-container form-label">
-                <label>Username </label>
-                <input type="email" className="form-control" name="uname" required />
-                {renderErrorMessage("uname")}
-              </div>
-            </div>
-          </div>
-          <div className="row justify-content-center align-items-center g-2">
-            <div className="col">
-              <div className="input-container form-label">
-                <label>Password </label>
-                <input type="password" className="form-control" name="pass" required />
-                {renderErrorMessage("pass")}
-              </div>
-            </div>
-            <div className="col">
-              <div className="input-container form-label">
-                <label>Password </label>
-                <input type="password" className="form-control" name="pass" required />
-                {renderErrorMessage("pass")}
-              </div>
-            </div>
-          </div>
-          <div className="row justify-content-center align-items-center g-2">
-            <div className="col">
-              <div className="input-container form-label">
-                <label>Password </label>
-                <input type="password" className="form-control" name="pass" required />
-                {renderErrorMessage("pass")}
-              </div>
-            </div>
-            <div className="col">
-              <div className="input-container form-label">
-                <label>Password </label>
-                <input type="password" className="form-control" name="pass" required />
-                {renderErrorMessage("pass")}
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="form-group">
-              <label htmlFor="">textarea</label>
-              <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
+  const handleSubmit = async (e) => {
+    console.log("handling submit..", e);
 
-          </div>
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-          </div>
-
-          <button className="btn btn-sumbit text-white btn-block w-100  fs-4">
-            {"Sumbit Message"}
-          </button>
-
-
-        </form>
-      </div>
-    </div>
-
-
-  );
+    e.preventDefault();
+  }
 
   return (
-    <div className="app">
-      <div className="login-form">
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
+
+    <div id='crmWebToEntityForm' className='zcwf_lblTopBottom crmWebToEntityForm' style='background-color: white;color: black;max-width: 900px;'>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      {/* <META HTTP-EQUIV='content-type' CONTENT='text/html;charset=UTF-8' /> */}
+      <form action='https://crm.zoho.com/crm/WebToLeadForm' method='POST' onSubmit='javascript:document.charset="UTF-8"; return checkMandatory5813716000000450005()' accept-charset='UTF-8'>
+        <input type='text' style='display:none;' name='xnQsjsdp' value='fbd9ac9ef083d47d156e2b601ccf851b002f8770fc457789a46bb8163f949f31'></input>
+        <input type='hidden' name='zc_gad' id='zc_gad' value=''></input>
+        <input type='text' style='display:none;' name='xmIwtLD' value='261ecd957ff9ba152b8b186a9bd4728895298b3fb3b654458c494237be5ca692'></input>
+        <input type='text' style='display:none;' name='actionType' value='TGVhZHM='></input>
+        <input type='text' style='display:none;' name='returnURL' value='https&#x3a;&#x2f;&#x2f;www.ansoftt.com&#x2f;' > </input>
+
+
+        <div className='zcwf_title' style='max-width: 900px;color: black; font-family:Arial;'>Get in touch</div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='First_Name'>First Name</label></div><div className='zcwf_col_fld'><input type='text' id='First_Name' name='First Name' maxlength='40'></input><div className='zcwf_col_help'> <span title='First Name' style='cursor: pointer; width: 16px; height: 16px; display: inline-block; background: #fff; border: 1px solid #c0c6cc; color: #c1c1c1; text-align: center; font-size: 11px; line-height: 16px; font-weight: bold; border-radius: 50%;' onclick='tooltipShow5813716000000450005(this)'>?</span><div className='zcwf_tooltip_over' style='display: none;'><span className='zcwf_tooltip_ctn'>First Name</span></div></div></div></div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='Last_Name'>Last Name<span style='color:red;'>*</span></label></div><div className='zcwf_col_fld'><input type='text' id='Last_Name' name='Last Name' maxlength='80'></input><div className='zcwf_col_help'></div></div></div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='Email'>Email</label></div><div className='zcwf_col_fld'><input type='text' ftype='email' id='Email' name='Email' maxlength='100'></input><div className='zcwf_col_help'></div></div></div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='Mobile'>Mobile</label></div><div className='zcwf_col_fld'><input type='text' id='Mobile' name='Mobile' maxlength='30'></input><div className='zcwf_col_help'></div></div></div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='Company'>Company<span style='color:red;'>*</span></label></div><div className='zcwf_col_fld'><input type='text' id='Company' name='Company' maxlength='200'></input><div className='zcwf_col_help'></div></div></div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='Designation'>Title</label></div><div className='zcwf_col_fld'><input type='text' id='Designation' name='Designation' maxlength='100'></input><div className='zcwf_col_help'></div></div></div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='City'>City</label></div><div className='zcwf_col_fld'><input type='text' id='City' name='City' maxlength='100'></input><div className='zcwf_col_help'></div></div></div>
+        <div className='zcwf_row'><div className='zcwf_col_lab' style='font-size:12px; font-family: Arial;'><label for='Description'>Description</label></div><div className='zcwf_col_fld'><textarea id='Description' name='Description'></textarea><div className='zcwf_col_help'></div></div></div><div className='zcwf_row'><div className='zcwf_col_lab'></div><div className='zcwf_col_fld'><input type='submit' id='formsubmit' className='formsubmit zcwf_button' value='Submit' title='Submit' onClick={handleSubmit} /><input type='reset' className='zcwf_button' name='reset' value='Reset' title='Reset'></input></div></div>
+
+      </form>
     </div>
+
+
   );
 }
 
